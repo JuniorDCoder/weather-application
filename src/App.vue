4fb20aa0ba99873ce26a95d367a1cfdb311dc515
JuniorDCoder <foryoungjuniorngu@gmail.com>
2024-03-06 13:40:50 +0100

    feat: New files initialized

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import { ref } from 'vue'
+  import SearchInput from './components/SearchInput.vue'
+  import WeatherCard from './components/WeatherCard.vue'
+
+  const places = ref([])
+  const addPlace = (data) => {
+    places.value.push(data)
+  }
+</script>
+
+<template>
+<main>
+  <!-- Date -->
+  <div class="text-center mb-6">
+    {{ 
+      new Date().toLocaleString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }) 
+    }}
+  </div>
+
+  <!-- Weather -->
+  <div>
+    <SearchInput @place-data="addPlace"/>
+
+  </div>
+
+  <!--Weather Cards-->
+  <div class="grid grid-cols-2 gap-4">
+    <div v-for="(place, idx) in places" :key="idx">
+      <WeatherCard :place="place" />
+    </div>
+  </div>
+</main>
+  
+</template>
